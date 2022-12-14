fa93ca0954e0def662d903ae7adff2a8b8b383b7
Jack Finnis <jack.finnis@icloud.com>
2022-12-14 00:04:12 +0000

    Initial Commit

---
+++ Boxes/BoxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxesApp.swift
+//  Boxes
+//
+//  Created by Jack Finnis on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BoxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
